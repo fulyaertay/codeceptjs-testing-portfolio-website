3c27dcbebb5a81e31a0131691850ce6bb80798b6
Fulya <fulya.ertay@gmail.com>
2023-10-27 12:42:22 +0300

    first commit

---
+++ codecept.conf.js
@@ -0,0 +1,24 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './tests',
+  output: './output',
+  helpers: {
+    Puppeteer: {
+      url: 'https://fulyaertay.netlify.app/',
+      show: true,
+      windowSize: '1200x900'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'codeceptjs-testing-portfolio-website'
+}
